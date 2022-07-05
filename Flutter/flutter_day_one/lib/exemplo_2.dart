18cd5da1e1715b82213eb748d7ebd67a9df43019
Gregory Viegas Zimmer <gregory.zimmer@warren.com.br>
2022-07-05 15:17:35 -0300

    [feat] Picture changed randomly by clicking at the button

--- Flutter/flutter_day_one/lib/exemplo_2.dart
+++ Flutter/flutter_day_one/lib/exemplo_2.dart
@@ -26,7 +26,7 @@ class MyWidget extends StatelessWidget {
     return Row(
       children: [
         Text(
-          'Hello',
+          'Oi',
           style: Theme.of(context).textTheme.headline4,
         ),
         Text(
--- Flutter/flutter_day_one/lib/main.dart
+++ Flutter/flutter_day_one/lib/main.dart
@@ -1,27 +1,50 @@
+import 'dart:math' as math;
+
 import 'package:flutter/material.dart';
 
 void main() {
   runApp(
     // Método que vai inflar nosso app (executar)
-    MaterialApp(
+    const MaterialApp(
       // um Widget que já vem com bastante coisas de configurações pronta
-      home: HomePage(),
+      home: HomePage(title: 'TesteBotao'),
+      debugShowCheckedModeBanner: false,
     ),
   );
 }
 
 class HomePage extends StatefulWidget {
   // Classe StatefulWidget (com estado),
+  final String title;
+  const HomePage({
+    Key? key,
+    required this.title,
+  }) : super(key: key);
   State<HomePage> createState() => _MyHomePageState();
 }
 
 class _MyHomePageState extends State<HomePage> {
+  int num = 0;
+
+  List<String> links = [
+    'https://cdn.pixabay.com/photo/2020/06/09/19/18/harry-potter-5279717_960_720.jpg',
+    'https://cdn.pixabay.com/photo/2012/06/19/10/32/owl-50267_960_720.jpg',
+    'https://cdn.pixabay.com/photo/2018/02/26/21/04/owl-3184032_960_720.jpg',
+    'https://cdn.pixabay.com/photo/2018/09/02/15/34/owl-3649048_960_720.jpg',
+  ];
+
+  void alterarImagem() {
+    setState(() {
+      num = math.Random().nextInt(links.length);
+    });
+  }
+
   Widget build(BuildContext context) {
     // Método que irpa "buildar" toda nossa view
     // passado o context (ajuda a identificar nossa árvore) e retornando um Widget
     return Scaffold(
       // Esqueleto do app, sempre intessante ter 1 em cada page
-      appBar: AppBar(), // Widget do próprio Flutter para AppBar
+      // appBar: AppBar(), // Widget do próprio Flutter para AppBar
       body: Center(
         // Widget para centralizar dentro do espaco disponível
         // child: Text('Olá mundo'), // Widget para mostrar um texto na tela
@@ -44,7 +67,7 @@ class _MyHomePageState extends State<HomePage> {
               ),
             ),
             Image.network(
-              'https://cdn.pixabay.com/photo/2020/06/09/19/18/harry-potter-5279717_960_720.jpg',
+              links[num],
               scale: 1.5,
             ),
             const SizedBox(height: 20),
@@ -53,7 +76,9 @@ class _MyHomePageState extends State<HomePage> {
                 primary: Colors.black,
                 padding: EdgeInsets.symmetric(horizontal: 50, vertical: 20),
               ),
-              onPressed: () {},
+              onPressed: () {
+                alterarImagem();
+              },
               child: const Text(
                 'Clica vai',
                 style: TextStyle(
@@ -68,5 +93,3 @@ class _MyHomePageState extends State<HomePage> {
     );
   }
 }
-
-//Image.network('https://cdn.pixabay.com/photo/2012/06/19/10/32/owl-50267_960_720.jpg');
